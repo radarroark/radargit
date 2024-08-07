9473d492605d0d944fae03b6a800bd65153b5076
radar roark <122068506+radarroark@users.noreply.github.com>
2024-08-07 18:51:39 -0400

    no need for custom expectEqual anymore

--- src/test.zig
+++ src/test.zig
@@ -6,10 +6,6 @@ const c = @cImport({
     @cInclude("git2.h");
 });
 
-pub fn expectEqual(expected: anytype, actual: anytype) !void {
-    try std.testing.expectEqual(@as(@TypeOf(actual), expected), actual);
-}
-
 test "end to end" {
     const temp_dir_name = "temp-test-end-to-end";
 
@@ -49,7 +45,7 @@ test "end to end" {
 
     // init repo
     var repo: ?*c.git_repository = null;
-    try expectEqual(0, c.git_repository_init(&repo, repo_path, 0));
+    try std.testing.expectEqual(0, c.git_repository_init(&repo, repo_path, 0));
     defer c.git_repository_free(repo);
 
     // make sure the git dir was created
@@ -70,23 +66,23 @@ test "end to end" {
 
         // add the files
         var index: ?*c.git_index = null;
-        try expectEqual(0, c.git_repository_index(&index, repo));
+        try std.testing.expectEqual(0, c.git_repository_index(&index, repo));
         defer c.git_index_free(index);
-        try expectEqual(0, c.git_index_add_bypath(index, "hello.txt"));
-        try expectEqual(0, c.git_index_add_bypath(index, "README"));
-        try expectEqual(0, c.git_index_write(index));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "hello.txt"));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "README"));
+        try std.testing.expectEqual(0, c.git_index_write(index));
 
         // make the commit
         var tree_oid: c.git_oid = undefined;
-        try expectEqual(0, c.git_index_write_tree(&tree_oid, index));
+        try std.testing.expectEqual(0, c.git_index_write_tree(&tree_oid, index));
         var tree: ?*c.git_tree = null;
-        try expectEqual(0, c.git_tree_lookup(&tree, repo, &tree_oid));
+        try std.testing.expectEqual(0, c.git_tree_lookup(&tree, repo, &tree_oid));
         defer c.git_tree_free(tree);
         var commit_oid: c.git_oid = undefined;
         var signature: ?*c.git_signature = null;
-        try expectEqual(0, c.git_signature_default(&signature, repo));
+        try std.testing.expectEqual(0, c.git_signature_default(&signature, repo));
         defer c.git_signature_free(signature);
-        try expectEqual(0, c.git_commit_create(
+        try std.testing.expectEqual(0, c.git_commit_create(
             &commit_oid,
             repo,
             "HEAD",
@@ -118,35 +114,35 @@ test "end to end" {
 
         // add the files
         var index: ?*c.git_index = null;
-        try expectEqual(0, c.git_repository_index(&index, repo));
+        try std.testing.expectEqual(0, c.git_repository_index(&index, repo));
         defer c.git_index_free(index);
-        try expectEqual(0, c.git_index_add_bypath(index, "LICENSE"));
-        try expectEqual(0, c.git_index_add_bypath(index, "CHANGELOG"));
-        try expectEqual(0, c.git_index_add_bypath(index, "hello.txt"));
-        try expectEqual(0, c.git_index_write(index));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "LICENSE"));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "CHANGELOG"));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "hello.txt"));
+        try std.testing.expectEqual(0, c.git_index_write(index));
 
         // get previous commit
         var parent_object: ?*c.git_object = null;
         var ref: ?*c.git_reference = null;
-        try expectEqual(0, c.git_revparse_ext(&parent_object, &ref, repo, "HEAD"));
+        try std.testing.expectEqual(0, c.git_revparse_ext(&parent_object, &ref, repo, "HEAD"));
         defer c.git_object_free(parent_object);
         defer c.git_reference_free(ref);
         var parent_commit: ?*c.git_commit = null;
-        try expectEqual(0, c.git_commit_lookup(&parent_commit, repo, c.git_object_id(parent_object)));
+        try std.testing.expectEqual(0, c.git_commit_lookup(&parent_commit, repo, c.git_object_id(parent_object)));
         defer c.git_commit_free(parent_commit);
         var parents = [_]?*c.git_commit{parent_commit};
 
         // make the commit
         var tree_oid: c.git_oid = undefined;
-        try expectEqual(0, c.git_index_write_tree(&tree_oid, index));
+        try std.testing.expectEqual(0, c.git_index_write_tree(&tree_oid, index));
         var tree: ?*c.git_tree = null;
-        try expectEqual(0, c.git_tree_lookup(&tree, repo, &tree_oid));
+        try std.testing.expectEqual(0, c.git_tree_lookup(&tree, repo, &tree_oid));
         defer c.git_tree_free(tree);
         var commit_oid: c.git_oid = undefined;
         var signature: ?*c.git_signature = null;
-        try expectEqual(0, c.git_signature_default(&signature, repo));
+        try std.testing.expectEqual(0, c.git_signature_default(&signature, repo));
         defer c.git_signature_free(signature);
-        try expectEqual(0, c.git_commit_create(
+        try std.testing.expectEqual(0, c.git_commit_create(
             &commit_oid,
             repo,
             "HEAD",
@@ -164,10 +160,10 @@ test "end to end" {
     {
         // init walker
         var walker: ?*c.git_revwalk = null;
-        try expectEqual(0, c.git_revwalk_new(&walker, repo));
+        try std.testing.expectEqual(0, c.git_revwalk_new(&walker, repo));
         defer c.git_revwalk_free(walker);
-        try expectEqual(0, c.git_revwalk_sorting(walker, c.GIT_SORT_TIME));
-        try expectEqual(0, c.git_revwalk_push_head(walker));
+        try std.testing.expectEqual(0, c.git_revwalk_sorting(walker, c.GIT_SORT_TIME));
+        try std.testing.expectEqual(0, c.git_revwalk_push_head(walker));
 
         // init commits list
         var commits = std.ArrayList(?*c.git_commit).init(allocator);
@@ -182,7 +178,7 @@ test "end to end" {
         var oid: c.git_oid = undefined;
         while (0 == c.git_revwalk_next(&oid, walker)) {
             var commit: ?*c.git_commit = null;
-            try expectEqual(0, c.git_commit_lookup(&commit, repo, &oid));
+            try std.testing.expectEqual(0, c.git_commit_lookup(&commit, repo, &oid));
             {
                 errdefer c.git_commit_free(commit);
                 try commits.append(commit);
@@ -190,7 +186,7 @@ test "end to end" {
         }
 
         // check the commit messages
-        try expectEqual(2, commits.items.len);
+        try std.testing.expectEqual(2, commits.items.len);
         try std.testing.expectEqualStrings("add license", std.mem.sliceTo(c.git_commit_message(commits.items[0]), 0));
         try std.testing.expectEqualStrings("let there be light", std.mem.sliceTo(c.git_commit_message(commits.items[1]), 0));
 
@@ -200,7 +196,7 @@ test "end to end" {
 
             const commit_oid = c.git_commit_tree_id(commit);
             var commit_tree: ?*c.git_tree = null;
-            try expectEqual(0, c.git_tree_lookup(&commit_tree, repo, commit_oid));
+            try std.testing.expectEqual(0, c.git_tree_lookup(&commit_tree, repo, commit_oid));
             defer c.git_tree_free(commit_tree);
 
             var prev_commit_tree: ?*c.git_tree = null;
@@ -208,22 +204,22 @@ test "end to end" {
             if (i < commits.items.len - 1) {
                 const prev_commit = commits.items[i + 1];
                 const prev_commit_oid = c.git_commit_tree_id(prev_commit);
-                try expectEqual(0, c.git_tree_lookup(&prev_commit_tree, repo, prev_commit_oid));
+                try std.testing.expectEqual(0, c.git_tree_lookup(&prev_commit_tree, repo, prev_commit_oid));
             }
             defer if (prev_commit_tree) |ptr| c.git_tree_free(ptr);
 
             var commit_diff: ?*c.git_diff = null;
-            try expectEqual(0, c.git_diff_tree_to_tree(&commit_diff, repo, prev_commit_tree, commit_tree, null));
+            try std.testing.expectEqual(0, c.git_diff_tree_to_tree(&commit_diff, repo, prev_commit_tree, commit_tree, null));
             defer c.git_diff_free(commit_diff);
 
             const delta_count = c.git_diff_num_deltas(commit_diff);
             for (0..delta_count) |delta_index| {
                 var commit_patch: ?*c.git_patch = null;
-                try expectEqual(0, c.git_patch_from_diff(&commit_patch, commit_diff, delta_index));
+                try std.testing.expectEqual(0, c.git_patch_from_diff(&commit_patch, commit_diff, delta_index));
                 defer c.git_patch_free(commit_patch);
 
                 var commit_buf: c.git_buf = std.mem.zeroes(c.git_buf);
-                try expectEqual(0, c.git_patch_to_buf(&commit_buf, commit_patch));
+                try std.testing.expectEqual(0, c.git_patch_to_buf(&commit_buf, commit_patch));
                 defer c.git_buf_dispose(&commit_buf);
             }
         }
@@ -265,23 +261,23 @@ test "end to end" {
 
         // add the files
         var index: ?*c.git_index = null;
-        try expectEqual(0, c.git_repository_index(&index, repo));
+        try std.testing.expectEqual(0, c.git_repository_index(&index, repo));
         defer c.git_index_free(index);
-        try expectEqual(0, c.git_index_add_bypath(index, "hello.txt"));
-        try expectEqual(0, c.git_index_add_bypath(index, "goodbye.txt"));
-        try expectEqual(0, c.git_index_remove_bypath(index, "LICENSE"));
-        try expectEqual(0, c.git_index_write(index));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "hello.txt"));
+        try std.testing.expectEqual(0, c.git_index_add_bypath(index, "goodbye.txt"));
+        try std.testing.expectEqual(0, c.git_index_remove_bypath(index, "LICENSE"));
+        try std.testing.expectEqual(0, c.git_index_write(index));
 
         // get status
         var status_list: ?*c.git_status_list = null;
         var status_options: c.git_status_options = undefined;
-        try expectEqual(0, c.git_status_options_init(&status_options, c.GIT_STATUS_OPTIONS_VERSION));
+        try std.testing.expectEqual(0, c.git_status_options_init(&status_options, c.GIT_STATUS_OPTIONS_VERSION));
         status_options.show = c.GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
         status_options.flags = c.GIT_STATUS_OPT_INCLUDE_UNTRACKED;
-        try expectEqual(0, c.git_status_list_new(&status_list, repo, &status_options));
+        try std.testing.expectEqual(0, c.git_status_list_new(&status_list, repo, &status_options));
         defer c.git_status_list_free(status_list);
         const entry_count = c.git_status_list_entrycount(status_list);
-        try expectEqual(6, entry_count);
+        try std.testing.expectEqual(6, entry_count);
 
         // loop over results
         for (0..entry_count) |i| {
@@ -318,24 +314,24 @@ test "end to end" {
         {
             // head oid
             var head_object: ?*c.git_object = null;
-            try expectEqual(0, c.git_revparse_single(&head_object, repo, "HEAD"));
+            try std.testing.expectEqual(0, c.git_revparse_single(&head_object, repo, "HEAD"));
             defer c.git_object_free(head_object);
             const head_oid = c.git_object_id(head_object);
 
             // commit
             var commit: ?*c.git_commit = null;
-            try expectEqual(0, c.git_commit_lookup(&commit, repo, head_oid));
+            try std.testing.expectEqual(0, c.git_commit_lookup(&commit, repo, head_oid));
             defer c.git_commit_free(commit);
 
             // commit tree
             const commit_oid = c.git_commit_tree_id(commit);
             var commit_tree: ?*c.git_tree = null;
-            try expectEqual(0, c.git_tree_lookup(&commit_tree, repo, commit_oid));
+            try std.testing.expectEqual(0, c.git_tree_lookup(&commit_tree, repo, commit_oid));
             defer c.git_tree_free(commit_tree);
 
             // diff
             var status_diff: ?*c.git_diff = null;
-            try expectEqual(0, c.git_diff_tree_to_workdir(&status_diff, repo, commit_tree, null));
+            try std.testing.expectEqual(0, c.git_diff_tree_to_workdir(&status_diff, repo, commit_tree, null));
             defer c.git_diff_free(status_diff);
 
             var modified_files = std.StringHashMap(void).init(allocator);
@@ -347,15 +343,15 @@ test "end to end" {
                 try modified_files.put(std.mem.sliceTo(delta.*.old_file.path, 0), {});
 
                 var patch: ?*c.git_patch = null;
-                try expectEqual(0, c.git_patch_from_diff(&patch, status_diff, delta_index));
+                try std.testing.expectEqual(0, c.git_patch_from_diff(&patch, status_diff, delta_index));
                 defer c.git_patch_free(patch);
 
                 var buf: c.git_buf = std.mem.zeroes(c.git_buf);
-                try expectEqual(0, c.git_patch_to_buf(&buf, patch));
+                try std.testing.expectEqual(0, c.git_patch_to_buf(&buf, patch));
                 defer c.git_buf_dispose(&buf);
             }
 
-            try expectEqual(4, modified_files.count());
+            try std.testing.expectEqual(4, modified_files.count());
             try std.testing.expect(modified_files.contains("LICENSE"));
             try std.testing.expect(modified_files.contains("hello.txt"));
             try std.testing.expect(modified_files.contains("README"));
@@ -384,5 +380,5 @@ test "end to end" {
 
     // focus on widget
     try root.getFocus().setFocus(leaf_id);
-    try expectEqual(leaf_id, root.getFocus().grandchild_id);
+    try std.testing.expectEqual(leaf_id, root.getFocus().grandchild_id);
 }
